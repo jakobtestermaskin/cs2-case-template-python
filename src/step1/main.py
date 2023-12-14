7ffd6d6e5f13806aec891e15e6a1f50bb23bba8c
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2023-12-14 14:59:12 +0100

    feat: get radio messages

--- src/step1/main.py
+++ src/step1/main.py
@@ -7,6 +7,10 @@ import json
 def handler(event, context):
     print("Handler was ran for g00")
 
-    print(event)
+    records = event['Records']
+
+    radio_messages = [x['SNS']['Message'] for x in event['Records']]
+
+    print(radio_messages)
 
     return {"statusCode": 200}
