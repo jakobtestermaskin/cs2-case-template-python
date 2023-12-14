da27c5e8b12cb74ed84d47433a53e7bd99769b21
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2023-12-14 15:06:19 +0100

    feat: json decode and get messages

--- src/step1/main.py
+++ src/step1/main.py
@@ -11,6 +11,9 @@ def handler(event, context):
 
     radio_messages = [x['Sns']['Message'] for x in event['Records']]
 
-    print(radio_messages)
+    events_from_radio_messages = json.loads(
+        [x['events'] for x in radio_messages])
+
+    print(events_from_radio_messages)
 
     return {"statusCode": 200}
