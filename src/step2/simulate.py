63352abf735d0e2aaca43aee22558699c7da7d09
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2024-02-08 22:59:35 +0100

    fix: typo in simulator

--- src/step2/simulate.py
+++ src/step2/simulate.py
@@ -2,4 +2,4 @@ from payment_service import handle
 
 
 if __name__ == "__main__":
-    handle({"song": "1 step forward 3 steps backwards", "artist": "oliva rodrigo"})
+    handle({"song": "1 step forward 3 steps backwards", "artist": "olivia rodrigo"})
